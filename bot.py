886d7266010791bd3d5905c6f2ccf055d6f588ca
SmileyAiOfficialDeveloper <21-00259@csa.edu.ph>
2026-09-20 21:48:40 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,4 +11,4 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Smiley is online as {bot.user}")
 
-bot.run(os.getenv("NEWDISCORDTOKEN"))
+bot.run(os.getenv("DISCORD_TOKEN"))
